9b6fa331063fdc36c95fc348bc701edb877f7b64
virakkhun <rakmanoban@gmail.com>
2022-08-16 09:02:05 +0700

    prefix: api

--- dist/routes/comment/comment.route.js
+++ dist/routes/comment/comment.route.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const prisma_instance_1 = require("../../utils/prisma.instance");
 async function commentRoutes(fastify, options) {
-    fastify.post("/comment/create", async (request, reply) => {
+    fastify.post("/api/comment/create", async (request, reply) => {
         const { comment, postId } = request.body;
         const newComment = await (0, prisma_instance_1.PrismaInstance)().comment.create({
             data: {
